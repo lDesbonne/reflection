4778c9afce633c2036fb573604eb0eb08f777912
lDesbonne <l.desbonne@gmail.com>
2017-06-17 15:05:24 +0100

    corrected JSON parsing of database data

--- reflection/proposal/AdminUtilities.py
+++ reflection/proposal/AdminUtilities.py
@@ -3,19 +3,19 @@ Created on 2 Jun 2017
 
 @author: Lennon
 '''
-from reflection.proposal.AdministrationServices import ApprovalData, ApprovalInfo
+from reflection.proposal.AdministrationServices import ProjectDataStructure
 
 approvalDataJsonTemplate = "{{\"name\":\"{name}\",\"children\":[{list}]}}"
 
 def approvalInfoToJsonString(approvalData):
-    if (type(approvalData) is ApprovalData):
+    if (type(approvalData) is ProjectDataStructure):
         parentDataList = ''
         if (len(approvalData.children) == 0):
             return approvalDataJsonTemplate.format(name = approvalData.name, list = '{"name": "None Found","size":1}')
         #For each child in the data
         for appData in approvalData.children:
             #Create a list of approval info
-            infoList = ",".join(str(appInfo[0]) for appInfo in appData.children)
+            infoList = ",".join(str(appInfo) for appInfo in appData.children)
             #remove the last comma from the info list
             parentDataList = ',' + parentDataList + approvalDataJsonTemplate.format(name = appData.name, list = infoList)
             
@@ -27,5 +27,5 @@ def approvalInfoToJsonString(approvalData):
         
         
     else:
-        raise Exception("Object must be of type ApprovalData")
+        raise Exception("Object must be of type ProjectDataStructure")
         
--- reflection/proposal/AdministrationServices.py
+++ reflection/proposal/AdministrationServices.py
@@ -6,28 +6,31 @@ Created on 20 May 2017
 from reflection.models import TopicAreas
 from reflection.models import ResearchProposals
 
-#Attempting to create the admin data using Objects
+# Attempting to create the admin data using Objects
 
 valididInitializationValues = ['Live', 'Pending']
-
+        
 class ApprovalData:
     def __init__(self, name):
         self.name = name
         self.children = []
     
     def __str__(self):
-        return '{"name":'+self.name+',"children":'+self.children+'}'
+        return '{"name":' + self.name + ',"children":' + self.children + '}'
             
         
-#Meta data should be added here to provide supplementary info
+# Meta data should be added here to provide supplementary info
 class ApprovalInfo:
     def __init__(self, name):
         self.name = name
         self.size = "1"
     
     def __str__(self):
-        return '{"name":"'+self.name+'","size":'+self.size+'}'
+        return '{"name":"' + self.name + '","size":' + self.size + '}'
         
+class ProjectDataStructure(ApprovalData):
+    def __init__(self, name):
+        ApprovalData.__init__(self, name)     
 
 def approvalDataBuilder(initialiser):
     
@@ -41,27 +44,27 @@ def approvalDataBuilder(initialiser):
             break
     
     if not validInitializer:
-        raise Exception("Initializer: "+initialiser+" invalid")
+        raise Exception("Initializer: " + initialiser + " invalid")
         
-        #Create parent data holder
-    dataTree = ApprovalData(name=initialiser + ' Topics')
+        # Create parent data holder
+    dataTree = ProjectDataStructure(name=initialiser + ' Topics')
     
-    #Get all the names of the topics using the active flag
+    # Get all the names of the topics using the active flag
     topicNames = TopicAreas.objects.filter(status=active)
     
-    #Create objects for those topics
+    # Create objects for those topics
     for topic in topicNames:
         questions = []
-        #Get all of the studies with the flag for those topics
+        # Get all of the studies with the flag for those topics
         studies = ResearchProposals.objects.filter(status=active, topic=topic)
-        #Add all the questions to the topic
+        # Add all the questions to the topic
         for question in studies:
             questions.append(ApprovalInfo(name=question.proposal))
         
         dataSet = ApprovalData(name=topic.topic_area)
-        dataSet.children.append(questions)
+        dataSet.children.extend(questions)
 
-        #Add to the dataTree
+        # Add to the dataTree
         dataTree.children.append(dataSet)
                     
     return dataTree
